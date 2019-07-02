530ae3aa1e4976b5e0b53c556dd89a78858ddb30
jeremy.b.smith <jeremy.b.smith@accenture.com>
2019-07-02 10:04:21 +1000

    setup.sh pretty printing

--- setup.sh
+++ setup.sh
@@ -1,7 +1,18 @@
+# Exit on error
 set -e
+
+printf -- '--------------------------------\n\tSETUP LOCAL PYENV.\n--------------------------------\n'
 pyenv local 3.6.0
+
+printf '\n--------------------------------\n\tLOCAL VENV.\n--------------------------------\n'
 python -m venv venv
+
+printf '\n--------------------------------\n\tACTIVATE VENV.\n--------------------------------\n'
 . venv/bin/activate
+
+printf '\n--------------------------------\n\tUPGRADE PIP.\n--------------------------------\n'
 pip install --upgrade pip
+
+printf '\n--------------------------------\n\tINSTALL REQS.\n--------------------------------\n'
 pip install -r requirements.txt
 
