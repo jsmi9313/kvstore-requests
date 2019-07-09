524a9f7e3ec996faacd3ec69fc2c6b514a060cfe
jeremy.b.smith <jeremy.b.smith@accenture.com>
2019-07-09 12:09:51 +1000

    updated setup/run scripts

---
+++ run.sh
@@ -0,0 +1,8 @@
+echo "Clear KV Store"
+./delete_all.py
+
+echo "Check Clear"
+./get_all.py
+
+echo "Post Data"
+./post_all.py
--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 # Exit on error
 set -e
 
-printf -- '--------------------------------\n\tSETUP LOCAL PYENV.\n--------------------------------\n'
-pyenv local 3.6.0
-
 printf '\n--------------------------------\n\tLOCAL VENV.\n--------------------------------\n'
-python -m venv venv
+python3 -m venv venv
 
 printf '\n--------------------------------\n\tACTIVATE VENV.\n--------------------------------\n'
 . venv/bin/activate
@@ -13,6 +10,5 @@ printf '\n--------------------------------\n\tACTIVATE VENV.\n------------------
 printf '\n--------------------------------\n\tUPGRADE PIP.\n--------------------------------\n'
 pip install --upgrade pip
 
-printf '\n--------------------------------\n\tINSTALL REQS.\n--------------------------------'
+printf '\n--------------------------------\n\tINSTALL REQS.\n--------------------------------\n'
 pip install -r requirements.txt
-
